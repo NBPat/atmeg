5f2d6a60460a3a348637bcbf398fbe088cfa903c
NBPat <50875140+NBPat@users.noreply.github.com>
2021-11-30 19:00:24 -0400

    Update Cport.c
    copy paste had left gaps, extra spaces

--- Cport.c
+++ Cport.c
@@ -1,12 +1,14 @@
 // count .c, Binary counting pattern on PORTB LEDs .
 // http://web.engr.oregonstate.edu/~traylor/ece473/beamer_lectures/debug_techniques.pdf
+#include <xc.h>
 #include<avr/io.h>
 #include<util/delay.h>
-int main () {
+int main(void) 
+    {
 	uint8_t i;
-	DDRB = 0xFF ; // set port B to all outputs
+	DDRB = 0xFF;
 	while (1){
 		PORTB ++;
-		for (i =0; i <=4; i ++){ _delay_ms (100);} // 0.5 sec delay
-	}// while
-}// main
+		for (i =0; i <=4; i ++){ _delay_ms (100);}
+	}
+}
